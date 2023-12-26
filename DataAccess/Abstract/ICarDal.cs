660d0fc792937bbb73a33a7a23cccadd75d145dd
İSMAİL ENES EROĞLU <73061163+firnox61@users.noreply.github.com>
2023-12-26 19:48:45 +0300

    Autofac

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal:IEntityRepository<Car>
+    public interface ICarDal : IEntityRepository<Car>
     {
 
         List<CarDetailDto> GetCarDetails();
